80b254f07aac02078485899f790c96f1d2835ee1
Hivenet-Nolan <144666332+Hivenet-Nolan@users.noreply.github.com>
2024-07-07 12:03:03 +0200

    update

--- src/infrastructure/logger/logger.interceptor.ts
+++ src/infrastructure/logger/logger.interceptor.ts
@@ -9,7 +9,7 @@ import { LoggerService } from "./services/logger.service";
 
 @Injectable()
 export class LoggerInterceptor implements NestInterceptor {
-  private readonly loggerSerivce: LoggerService;
+  private readonly loggerSerivce = new LoggerService();
 
   intercept(context: ExecutionContext, next: CallHandler): Observable<void> {
     const now = Date.now();
